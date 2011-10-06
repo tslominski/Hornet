75ae7350f1e3b782a22114bbcf66bf03b9560123
Tomasz Słomiński <tomasz.slominski@gmail.com>
2011-10-06 15:43:20 +0200

    PhpMessDetector added to tests

--- dev/run-tests.sh
+++ dev/run-tests.sh
@@ -1,2 +1,3 @@
 DIR="$( cd -P "$( dirname "$0" )" && pwd )"
 phpunit --coverage-html $DIR/results/coverage --colors $DIR/tests/Hornet/Data/Entities/URI.php 
+phpmd $DIR/../lib html codesize,unusedcode,naming,design > $DIR/results/mess/index.html
